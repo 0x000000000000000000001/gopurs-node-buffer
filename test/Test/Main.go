9880ba05b8f70767174d2e89938a0fb68685396e
0x1 <kevin.francart3@gmail.com>
2026-08-09 01:35:28 +0200

    Add bin/test and test infrastructure

---
+++ test/Test/Main.go
@@ -0,0 +1,9 @@
+package Test_Main
+
+import (
+	_ "gopurs/output/Effect.Console"
+	_ "gopurs/output/Test.Assert"
+	"gopurs/output/gopurs_runtime"
+)
+
+var InitFFI = gopurs_runtime.Value{}
--- test/Test/Main.purs
+++ test/Test/Main.purs
@@ -4,5 +4,7 @@ import Prelude
 import Effect (Effect)
 import Test.Node.Buffer as Buffer
 
+foreign import initFFI :: Unit
+
 main :: Effect Unit
 main = Buffer.test
